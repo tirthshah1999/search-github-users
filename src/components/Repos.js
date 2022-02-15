61366e407bb994e75f267b33481dae83242c4b35
Tirth <tirthcoder@gmail.com>
2022-02-15 11:21:02 +0530

    implemented charts

--- src/components/Repos.js
+++ src/components/Repos.js
@@ -1,9 +1,60 @@
 import React from 'react';
+import { useContext } from 'react';
 import styled from 'styled-components';
 import { GithubContext } from '../context/context';
 import { ExampleChart, Pie3D, Column3D, Bar3D, Doughnut2D } from './Charts';
+
 const Repos = () => {
-  return <h2>repos component</h2>;
+  const {repos} = useContext(GithubContext);
+  // Count used languages and stars
+  const languages = repos.reduce((total, item) => {
+    const {language, stargazers_count} = item;
+    if(!language) return total;
+    
+    if(!total[language]){
+      total[language] = {label: language, value: 1, stars: stargazers_count}
+    }else{
+      total[language] = {...total[language], value: total[language].value + 1, stars: total[language].stars + stargazers_count}
+    }
+
+    return total;
+  },{})
+
+  
+  // Find 5 most used language among all of them
+  const mostUsed = Object.values(languages).sort((a,b) => {
+    return b.value - a.value;
+  }).slice(0, 5);
+  console.log(mostUsed);
+
+  // most stars per language -- Chart used value to display, but we want start count so use map
+  const mostPopular = Object.values(languages).sort((a,b) => {
+    return b.stars - a.stars
+  }).map((item) => {
+    return {...item, value: item.stars}
+  }).slice(0, 5);
+
+  // stars, forks
+  let {stars, forks} = repos.reduce((total, item) => {
+    const { stargazers_count, name, forks } = item;
+    total.stars[stargazers_count] = {label: name, value: stargazers_count};
+    total.forks[forks] = {label: name, value: forks};
+    return total;
+  }, {stars: {}, forks: {}})
+
+  stars = Object.values(stars).slice(-5).reverse();
+  forks = Object.values(forks).slice(-5).reverse();
+
+  return (
+    <section className="section">
+      <Wrapper className='section-center'>
+        <Pie3D data={mostUsed} />
+        <Column3D data={stars} />
+        <Doughnut2D data={mostPopular} />
+        <Bar3D data={forks} />
+      </Wrapper>
+    </section>
+  )
 };
 
 const Wrapper = styled.div`
