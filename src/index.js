da90106fda3a4152fb8260f83bc7bac296b99130
Tirth <tirthcoder@gmail.com>
2022-02-17 10:47:36 +0530

    authentication completed

--- src/index.js
+++ src/index.js
@@ -6,11 +6,22 @@ import * as serviceWorker from './serviceWorker';
 import { GithubProvider } from './context/context';
 import { Auth0Provider } from '@auth0/auth0-react';
 
+// dev-jds83me3.us.auth0.com
+// MWPn0eDZGhkUtz8dXltPs9bIuZ6Zikrl
+
 ReactDOM.render(
   <React.StrictMode>
-    <GithubProvider>
-      <App />
-    </GithubProvider>
+    <Auth0Provider
+      domain= {process.env.REACT_APP_DOMAIN}
+      clientId= {process.env.REACT_APP_CLIENT_ID}
+      redirectUri= {window.location.origin}
+      // If you login with social account, and then you go to wrong page, when you click back to home you will be redirected to login page so to prevent that we have to store social login in localStorage (Note:- This will not happen while simple login)
+      cacheLocation='localstorage'
+    >
+      <GithubProvider>
+        <App />
+      </GithubProvider>
+    </Auth0Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
--- src/pages/AuthWrapper.js
+++ src/pages/AuthWrapper.js
@@ -2,8 +2,28 @@ import React from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
 import loadingGif from '../images/preloader.gif';
 import styled from 'styled-components';
-function AuthWrapper() {
-  return <h2>authwrapper component</h2>;
+
+// You need this to work auth0 properly, because if you are in loading or error state, it will not redirect to dashboard even though you had login successfully.
+
+function AuthWrapper({children}) {
+  const {isLoading, error} = useAuth0();
+  if(isLoading){
+    return(
+      <Wrapper>
+        <img src={loadingGif} alt="spinner" />
+      </Wrapper>
+    )
+  }
+
+  if(error){
+    return(
+      <Wrapper>
+        <h1>{error.message}</h1>
+      </Wrapper>
+    )
+  }
+
+  return <>{children}</>;
 }
 
 const Wrapper = styled.section`
